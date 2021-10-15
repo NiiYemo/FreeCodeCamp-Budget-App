aa67b76c33bb86b8d7cf187c90a70168ded425fe
Nii Yemo <niiyemo@yahoo.co.uk>
2021-10-15 20:27:09 +0100

    1st iteration

--- budget.py
+++ budget.py
@@ -5,22 +5,44 @@ class Category:
     
     def __init__(self, budget_category):
         self.budget_category = budget_category
+        
+    def get_balance(self):
+        accumulated_total = 0
+        for i in self.ledger:
+            accumulated_total += i["amount"]
 
-    def deposit(self, amount, description):        
+        return accumulated_total
+      
+    def deposit(self, amount,   description): 
         self.ledger.append({"amount": amount, "description": description})
-        
-    
+           
     def withdraw(self, amount, description):
-        self.ledger.append({"amount": -amount, "description": description})
-
-    
-    def transfer(self, amount, budget_category):
-        self.withdraw("Transfer to "+budget_category)
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        else: 
+            return False
+        
+    def transfer(self, amount, destination_obj):
+        if self.check_funds(amount):
+            self.withdraw(amount, "Transfer to " + destination_obj.budget_category)
+            destination_obj.deposit(amount, "Transfer from " + self.budget_category)
+            return True
+        else:
+            return False
         
         
+    def check_funds(self, amount):
+        if self.get_balance() > amount:
+            return False
+        else:
+            return True 
     
-    def check_funds(self):
-        pass
+    def display(self):
+        print("*************Food*************")
+        for i in self.ledger:
+            print(i["description"] + str(i["amount"])+"\n")
+        print("Total: "+self.get_balance)
     
-    def create_spend_chart(categories):
-        pass
+def create_spend_chart(self, categories):
+    pass
