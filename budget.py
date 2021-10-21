21d9b5deab2449e2fab0f33f23833c937d11e8e3
Nii Yemo <niiyemo@yahoo.co.uk>
2021-10-21 12:15:24 +0100

    Creating graph

--- budget.py
+++ budget.py
@@ -1,19 +1,25 @@
 class Category:
     
     # class attribute 
-    ledger = []
+    
     
     def __init__(self, budget_category):
         self.budget_category = budget_category
-        
+        self.ledger = []
+    
+    def __repr__(self):
+        string = "*************{self.budget_category}*************\n"
+        for i in self.ledger:
+            string += i["description"] + str(i["amount"])+"\n"
+        print("Total: "+self.get_balance)
+    
     def get_balance(self):
         accumulated_total = 0
         for i in self.ledger:
-            accumulated_total += i["amount"]
-
+            accumulated_total += float(i["amount"])
         return accumulated_total
       
-    def deposit(self, amount,   description): 
+    def deposit(self, amount, description): 
         self.ledger.append({"amount": amount, "description": description})
            
     def withdraw(self, amount, description):
@@ -31,9 +37,8 @@ class Category:
         else:
             return False
         
-        
     def check_funds(self, amount):
-        if self.get_balance() > amount:
+        if self.get_balance() < amount:
             return False
         else:
             return True 
@@ -43,6 +48,41 @@ class Category:
         for i in self.ledger:
             print(i["description"] + str(i["amount"])+"\n")
         print("Total: "+self.get_balance)
+
+food = Category("Food")
+entertainment = Category("Entertainment")
+food.deposit(100,"Pizza Party")
+food.transfer(25, entertainment)
+print(entertainment.ledger)
+print(food.ledger)
+
+print(food.get_balance())
+print(entertainment.get_balance())
+""""""
+def create_spend_chart(categories):
+    calculate_percentages(categories)
     
-def create_spend_chart(self, categories):
-    pass
+def calculate_percentages(categories):
+    
+    total_withdrawals = 0
+    dictionary = {}
+    
+    # Get total withdrawals 
+    for i in categories:
+        category_ledger = i.ledger
+        total_category_withdrawals = 0
+        
+        for j in category_ledger:
+            if j["amount"] < 0: total_category_withdrawals += j["amount"]
+        
+        dictionary[i.budget_category] = abs(total_category_withdrawals) 
+        total_withdrawals += abs(total_category_withdrawals) 
+        
+    #Calculate Percenteges 
+    for i in dictionary:
+        dictionary[i] =  int((dictionary[i]/total_withdrawals) * 100)
+    
+    return dictionary
+    
+
+create_spend_chart([food, entertainment])
